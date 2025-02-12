7c02950018dc5e2fe088e0e2f965de22fb391756
SwiichyCode <adlpromail@gmail.com>
2025-02-12 10:42:02 +0100

    refact: code spliting

--- src/modules/authentication/authentication-email-verification-card/authentication-verification-header.tsx
+++ src/modules/authentication/authentication-email-verification-card/authentication-verification-header.tsx
@@ -1,11 +1,15 @@
 import { CardDescription } from '@/core/presentation/components/common/ui/card';
 import { CardHeader, CardTitle } from '@/core/presentation/components/common/ui/card';
+import * as SignIn from '@clerk/elements/sign-in';
 
 export const AuthenticationVerificationHeader = () => {
   return (
     <CardHeader>
-      <CardTitle>Verify your email</CardTitle>
-      <CardDescription>Use the verification link sent to your email address</CardDescription>
+      <CardTitle>Check your email</CardTitle>
+      <CardDescription>Enter the verification code sent to your email</CardDescription>
+      <p className="text-sm text-muted-foreground">
+        Welcome back <SignIn.SafeIdentifier />
+      </p>
     </CardHeader>
   );
 };
--- src/modules/authentication/authentication-password-strategy/_index.tsx
+++ src/modules/authentication/authentication-password-strategy/_index.tsx
@@ -1,17 +1,10 @@
-import { Icons } from '@/core/presentation/components/common/icons/_index';
-import { Button } from '@/core/presentation/components/common/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/core/presentation/components/common/ui/card';
-import { Input } from '@/core/presentation/components/common/ui/input';
-import { Label } from '@/core/presentation/components/common/ui/label';
-import * as Clerk from '@clerk/elements/common';
+import { Card, CardContent, CardFooter } from '@/core/presentation/components/common/ui/card';
 import * as SignIn from '@clerk/elements/sign-in';
+
+import { AuthenticationChooseStrategy } from './authentication-choose-strategy';
+import { AuthenticationPasswordField } from './authentication-password-field';
+import { AuthenticationPasswordHeader } from './authentication-password-header';
+import { AuthenticationPasswordSubmit } from './authentication-password-submit';
 
 type AuthenticationPasswordStrategyProps = {
   isGlobalLoading: boolean;
@@ -21,40 +14,14 @@ export const AuthenticationPasswordStrategy = ({ isGlobalLoading }: Authenticati
   return (
     <SignIn.Strategy name="password">
       <Card className="w-full sm:w-96">
-        <CardHeader>
-          <CardTitle>Check your email</CardTitle>
-          <CardDescription>Enter the verification code sent to your email</CardDescription>
-          <p className="text-sm text-muted-foreground">
-            Welcome back <SignIn.SafeIdentifier />
-          </p>
-        </CardHeader>
+        <AuthenticationPasswordHeader />
         <CardContent className="grid gap-y-4">
-          <Clerk.Field name="password" className="space-y-2">
-            <Clerk.Label asChild>
-              <Label>Password</Label>
-            </Clerk.Label>
-            <Clerk.Input type="password" asChild>
-              <Input />
-            </Clerk.Input>
-            <Clerk.FieldError className="block text-sm text-destructive" />
-          </Clerk.Field>
+          <AuthenticationPasswordField />
         </CardContent>
         <CardFooter>
           <div className="grid w-full gap-y-4">
-            <SignIn.Action submit asChild>
-              <Button disabled={isGlobalLoading}>
-                <Clerk.Loading>
-                  {isLoading => {
-                    return isLoading ? <Icons.spinner className="size-4 animate-spin" /> : 'Continue';
-                  }}
-                </Clerk.Loading>
-              </Button>
-            </SignIn.Action>
-            <SignIn.Action navigate="choose-strategy" asChild>
-              <Button type="button" size="sm" variant="link">
-                Use another method
-              </Button>
-            </SignIn.Action>
+            <AuthenticationPasswordSubmit isGlobalLoading={isGlobalLoading} />
+            <AuthenticationChooseStrategy />
           </div>
         </CardFooter>
       </Card>
