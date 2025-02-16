a9e77f55f2fd880557a9fcee9b1b0411b39b6f5b
SwiichyCode <adlpromail@gmail.com>
2025-02-16 03:26:34 +0100

    refactor: remove comments

--- dependency_injection/modules/webhook.module.ts
+++ dependency_injection/modules/webhook.module.ts
@@ -7,17 +7,14 @@ import { createModule } from '@evyweb/ioctopus';
 export function createWebhookModule() {
   const webhookModule = createModule();
 
-  // Configuration du handler de souscription
   webhookModule
     .bind(DI_SYMBOLS.StripeSubscriptionCreatedHandler)
     .toClass(StripeSubscriptionCreatedHandler, [DI_SYMBOLS.SubscriptionService, DI_SYMBOLS.CrashReporterService]);
 
-  // Configuration du tableau des handlers d'événements
   webhookModule.bind(DI_SYMBOLS.WebhookEventHandlers).toFactory(() => {
     return [ApplicationContainer.get(DI_SYMBOLS.StripeSubscriptionCreatedHandler)];
   });
 
-  // Configuration du handler principal de webhook
   webhookModule
     .bind(DI_SYMBOLS.StripeWebhookHandler)
     .toClass(StripeWebhookHandler, [DI_SYMBOLS.WebhookEventHandlers, DI_SYMBOLS.CrashReporterService]);
